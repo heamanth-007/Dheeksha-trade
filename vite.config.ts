4c49d9b29e8fd53a91eed1a4ee3824179d9398b4
heamanth-007 <heamanthprabhu59@gmail.com>
2026-08-28 12:25:04 +0530

    chore: configure Vercel deployment, environment variables, and Vite build chunking optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui') || id.includes('@emotion')) {
+              return 'mui';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
+
